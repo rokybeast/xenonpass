f90bf50bbace9cc2ad35e34152e17e55d8a1c8c8
Mohammed Sajid Shaik <sajid.shaik1186@gmail.com>
2026-04-04 14:36:28 +0530

    chore: move vault module to src and update build configurations

--- build.rs
+++ build.rs
@@ -3,10 +3,10 @@ use std::env;
 fn main() {
     pkg_config::Config::new().probe("libsodium").unwrap();
     cc::Build::new()
-        .file("vault/kdf.c")
-        .file("vault/crypto.c")
-        .file("vault/io.c")
-        .file("vault/mem.c")
-        .include("vault/include")
+        .file("src/vault/kdf.c")
+        .file("src/vault/crypto.c")
+        .file("src/vault/io.c")
+        .file("src/vault/mem.c")
+        .include("src/vault/include")
         .compile("xpass");
 }
--- vault/mem.c
+++
@@ -1,9 +0,0 @@
-#include "xpass.h"
-
-int xpass_init(void) {
-    return sodium_init();
-}
-
-void xpass_secure_zero(void *buf, size_t len) {
-    sodium_memzero(buf, len);
-}
