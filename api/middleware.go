59d10351dec9996f0fb2cd63cd2174cb8d599b41
Mohammed Sajid Shaik <sajid.shaik1186@gmail.com>
2026-04-28 15:02:45 +0530

    feat: add CORS middleware

---
+++ api/middleware.go
@@ -0,0 +1,57 @@
+package api
+
+import "net/http"
+
+type CORSConfig struct {
+	AllowOrigins []string
+	AllowMethods []string
+	AllowHeaders []string
+}
+
+func DefaultCORSConfig() CORSConfig {
+	return CORSConfig{
+		AllowOrigins: []string{"http://localhost:5173"},
+		AllowMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders: []string{"Content-Type", "Authorization"},
+	}
+}
+
+func CORSMiddleware(cfg CORSConfig) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			origin := r.Header.Get("Origin")
+			allowed := false
+			for _, o := range cfg.AllowOrigins {
+				if o == origin || o == "*" {
+					allowed = true
+					break
+				}
+			}
+
+			if allowed {
+				w.Header().Set("Access-Control-Allow-Origin", origin)
+			}
+
+			w.Header().Set("Access-Control-Allow-Methods", joinStrings(cfg.AllowMethods, ", "))
+			w.Header().Set("Access-Control-Allow-Headers", joinStrings(cfg.AllowHeaders, ", "))
+
+			if r.Method == http.MethodOptions {
+				w.WriteHeader(http.StatusNoContent)
+				return
+			}
+
+			next.ServeHTTP(w, r)
+		})
+	}
+}
+
+func joinStrings(s []string, sep string) string {
+	if len(s) == 0 {
+		return ""
+	}
+	result := s[0]
+	for _, v := range s[1:] {
+		result += sep + v
+	}
+	return result
+}
