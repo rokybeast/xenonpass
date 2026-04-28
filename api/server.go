d31e17115a288d90bb3b8535582cf10854619e9f
Mohammed Sajid Shaik <sajid.shaik1186@gmail.com>
2026-04-28 15:03:10 +0530

    feat: add HTTP server with route wiring

---
+++ api/server.go
@@ -0,0 +1,48 @@
+package api
+
+import "net/http"
+
+type Server struct {
+	Addr   string
+	mux    *http.ServeMux
+	store  *VaultStore
+	cors   CORSConfig
+}
+
+func NewServer(addr string) *Server {
+	s := &Server{
+		Addr:  addr,
+		mux:   http.NewServeMux(),
+		store: NewVaultStore(),
+		cors:  DefaultCORSConfig(),
+	}
+	s.routes()
+	return s
+}
+
+func NewServerWithCORS(addr string, cors CORSConfig) *Server {
+	s := &Server{
+		Addr:  addr,
+		mux:   http.NewServeMux(),
+		store: NewVaultStore(),
+		cors:  cors,
+	}
+	s.routes()
+	return s
+}
+
+func (s *Server) routes() {
+	s.mux.HandleFunc("/api/health", HandleHealth())
+	s.mux.HandleFunc("/api/entries", HandleListEntries(s.store))
+	s.mux.HandleFunc("/api/entries/create", HandleCreateEntry(s.store))
+	s.mux.HandleFunc("/api/entries/get", HandleGetEntry(s.store))
+	s.mux.HandleFunc("/api/entries/delete", HandleDeleteEntry(s.store))
+}
+
+func (s *Server) Handler() http.Handler {
+	return CORSMiddleware(s.cors)(s.mux)
+}
+
+func (s *Server) ListenAndServe() error {
+	return http.ListenAndServe(s.Addr, s.Handler())
+}
